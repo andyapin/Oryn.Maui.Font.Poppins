3a288085388de37c351d9040a00610e820602a50
Ibnu Hajar Handityo <andyapin@gmail.com>
2025-07-24 09:38:49 +0700

    refactor

--- src/FontExtensions.cs
+++ src/FontExtensions.cs
@@ -1,38 +1,43 @@
 ﻿namespace Plugin.Toolkit.Fonts.Poppins
 {
+    /// <summary>
+    /// Provides extension methods for adding Poppins fonts to an IFontCollection.
+    /// </summary>
     public static class FontExtensions
     {
         /// <summary>
         /// Add font to global. using: FontFamily="Regular" *(Italic, Medium, SemiBold, Bold, etc..)
         /// </summary>
+        /// <param name="fonts">The font collection to add fonts to.</param>
+        /// <returns>The updated font collection.</returns>
         public static IFontCollection AddPoppinsFonts(this IFontCollection fonts)
         {
-            var thisAssembly = typeof(FontExtensions).Assembly;
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsRegular.ttf", FontAliases.PoppinsRegular);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsItalic.ttf", FontAliases.PoppinsItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsMedium.ttf", FontAliases.PoppinsMedium);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsMediumItalic.ttf", FontAliases.PoppinsMediumItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsSemiBold.ttf", FontAliases.PoppinsSemiBold);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsSemiBoldItalic.ttf", FontAliases.PoppinsSemiBoldItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsBold.ttf", FontAliases.PoppinsBold);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsBoldItalic.ttf", FontAliases.PoppinsBoldItalic);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsRegular)}.ttf", FontAliases.PoppinsRegular);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsItalic)}.ttf", FontAliases.PoppinsItalic);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsMedium)}.ttf", FontAliases.PoppinsMedium);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsMediumItalic)}.ttf", FontAliases.PoppinsMediumItalic);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsSemiBold)}.ttf", FontAliases.PoppinsSemiBold);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsSemiBoldItalic)}.ttf", FontAliases.PoppinsSemiBoldItalic);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsBold)}.ttf", FontAliases.PoppinsBold);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsBoldItalic)}.ttf", FontAliases.PoppinsBoldItalic);
             return fonts;
         }
 
         /// <summary>
         /// Add font to partial. using: FontFamily="PoppinsRegular" *(PoppinsItalic, PoppinsMedium, PoppinsSemiBold, PoppinsBold, etc..)
         /// </summary>
+        /// <param name="fonts">The font collection to add fonts to.</param>
+        /// <returns>The updated font collection.</returns>
         public static IFontCollection AddPoppinsFontsPartial(this IFontCollection fonts)
         {
-            var thisAssembly = typeof(FontExtensions).Assembly;
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsRegular.ttf", FontAliases.Name + FontAliases.PoppinsRegular);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsItalic.ttf", FontAliases.Name + FontAliases.PoppinsItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsMedium.ttf", FontAliases.Name + FontAliases.PoppinsMedium);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsMediumItalic.ttf", FontAliases.Name + FontAliases.PoppinsMediumItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsSemiBold.ttf", FontAliases.Name + FontAliases.PoppinsSemiBold);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsSemiBoldItalic.ttf", FontAliases.Name + FontAliases.PoppinsSemiBoldItalic);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsBold.ttf", FontAliases.Name + FontAliases.PoppinsBold);
-            fonts.AddEmbeddedResourceFont(thisAssembly, "PoppinsBoldItalic.ttf", FontAliases.Name + FontAliases.PoppinsBoldItalic);
+            fonts.AddFont($"{nameof(FontAliases.PoppinsRegular)}.ttf", nameof(FontAliases.PoppinsRegular));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsItalic)}.ttf", nameof(FontAliases.PoppinsItalic));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsMedium)}.ttf", nameof(FontAliases.PoppinsMedium));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsMediumItalic)}.ttf", nameof(FontAliases.PoppinsMediumItalic));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsSemiBold)}.ttf", nameof(FontAliases.PoppinsSemiBold));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsSemiBoldItalic)}.ttf", nameof(FontAliases.PoppinsSemiBoldItalic));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsBold)}.ttf", nameof(FontAliases.PoppinsBold));
+            fonts.AddFont($"{nameof(FontAliases.PoppinsBoldItalic)}.ttf", nameof(FontAliases.PoppinsBoldItalic));
             return fonts;
         }
     }
